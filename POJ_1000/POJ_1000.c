2bd338aab7b2cf292bff5497e6d72035e62fb25d
Aaron Chang <a020304888a@gmail.com>
2016-11-13 05:35:45 -0800

    Add POJ_1000

---
+++ POJ_1000/POJ_1000.c
@@ -0,0 +1,14 @@
+/* Name: POJ_1000.c
+   Purpose: A+B problem.
+   Auther: T. W. Chang */
+
+#include<stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	printf("%d", a + b);
+	return 0;
+}
